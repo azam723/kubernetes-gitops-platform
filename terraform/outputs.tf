f1dc8d9bf0115a679a80978ba2dedbf165bd69b7
khan <azam.mohammed3@icloud.com>
2026-09-15 17:37:05 +0000

    prepare AWS VPC for EKS

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the project VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "availability_zones" {
+  description = "Availability zones used by the project"
+  value       = data.aws_availability_zones.available.names
+}
